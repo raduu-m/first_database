b585be9b9c0d01c4fd480bade9be42b80c167010
Radu M <radu.moldoveanuf@gmail.com>
2022-09-08 09:53:53 +0300

    Fixed Security issues

--- main.py
+++ main.py
@@ -2,18 +2,19 @@ from dotenv import load_dotenv, find_dotenv
 import os
 import pprint
 from pymongo import MongoClient
-load_dotenv(find_dotenv())
-
-
-
+load_dotenv()
 import certifi
 
-client =MongoClient("mongodb+srv://test:test@rm21.dgycuig.mongodb.net/?retryWrites=true&w=majority",tlsCAFile=certifi.where())
+password=os.getenv("PSWD")
+
+
+client =MongoClient(f"mongodb+srv://test:{password}@rm21.dgycuig.mongodb.net/?retryWrites=true&w=majority",tlsCAFile=certifi.where())
+
 db = client.test
 
 dbs=client.list_database_names() 
 print(dbs)
 
 # view all items in the collection
-# for item in db.test.find():
-#     print(item)
+for item in db.test.find():
+    print(item)
