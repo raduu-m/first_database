55a9788707ebd3cd4ca716688dd786613df9eaa7
Radu M <radu.moldoveanuf@gmail.com>
2022-09-08 09:32:04 +0300

    SSL Certificate Bug Fix

--- t.py
+++ t.py
@@ -4,6 +4,16 @@ import pprint
 from pymongo import MongoClient
 load_dotenv(find_dotenv())
 
-password=os.environ.get('MONGODB_PWD')
-connection_string=f"mongodb+srv://radu:{password}@rm21.dgycuig.mongodb.net/?retryWrites=true&w=majority"
-client=MongoClient(connection_string)
+
+
+import certifi
+
+client =MongoClient("mongodb+srv://test:test@rm21.dgycuig.mongodb.net/?retryWrites=true&w=majority",tlsCAFile=certifi.where())
+db = client.test
+
+dbs=client.list_database_names() 
+print(dbs)
+
+# view all items in the collection
+# for item in db.test.find():
+#     print(item)
